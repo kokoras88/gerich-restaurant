5eedea7db6d864ce8a6b838a913f8fdf615450f6
Alexandros Georgiadis <kokoras19888@gmail.com>
2024-02-13 16:30:35 +0100

    added some new sections

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className='app__bg app__wrapper section__padding' id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact"/>
+      <h1 className="headtext__cormorant" style={{marginBottom: "3rem"}}>Find Us</h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">Lane Ends Bungalow, Whatcroft Hall Lane, Rudheathg, CW9 75G</p>
+      </div>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
